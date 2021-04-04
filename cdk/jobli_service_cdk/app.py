3010b769fb09be72c4dc5f952c2b1f79c2157623
Or Zimon <or.zimon@cyberark.com>
2021-04-04 14:28:55 +0300

    EV-0000:initial cdk template

--- cdk/jobli_service_cdk/app.py
+++ cdk/jobli_service_cdk/app.py
@@ -5,15 +5,15 @@
 import os
 from aws_cdk import core
 from boto3 import client, session
+from jobli_service_cdk.service_stack.jobli_construct import get_stack_name
 from jobli_service_cdk.service_stack.jobli_stack import JobliStack
-from stack_utils.stack_name import get_stack_name
 from jobli_service_cdk.service_stack.constants import BASE_NAME
 
 account = client('sts').get_caller_identity()['Account']
 region = session.Session().region_name
 app = core.App()
 jobli_stack = JobliStack(
-    app, get_stack_name(BASE_NAME),
+    app, get_stack_name(),
     env=core.Environment(account=os.environ.get("AWS_DEFAULT_ACCOUNT", account), region=os.environ.get("AWS_DEFAULT_REGION", region)))
 
 app.synth()

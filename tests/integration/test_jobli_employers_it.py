ef6db2d299c86ab71d4ea8fee5f0eefefff73ce9
yyaffe <60100068+yyaffe@users.noreply.github.com>
2021-10-18 08:05:22 +0300

    Load test data (#26)
    load employers and jobs from file, for tests purposes

--- tests/integration/test_jobli_employers_it.py
+++ tests/integration/test_jobli_employers_it.py
@@ -1,10 +1,15 @@
-# pylint: disable = print-used
+import random
+import string
+import json
+
 import requests
 import os
 from datetime import datetime, timedelta
 from http import HTTPStatus
 import pytest
 from dotenv import load_dotenv
+from pydantic.networks import HttpUrl
+from service.models.employer.employer_job import EmployerJob, JobScope
 from tests.helpers.environment_handler import load_env_vars
 from tests.helpers.cognito_auth_util import add_auth_header
 from service.models.employer.employer import Employer
@@ -33,6 +38,97 @@ def endpoint_url():
 @pytest.fixture(scope="module")
 def auth_headers():
     return add_auth_header()
+
+
+def get_random_email(name_length: int, domain: str) -> str:
+    return ''.join(random.choice(string.ascii_letters) for x in range(name_length)) + domain
+
+
+def get_employer_by_name(business_name: str, existing_employers: List[Employer]) -> Employer:
+    for employer in existing_employers:
+        if employer.business_name == business_name:
+            return employer
+    return None
+
+
+def test_create_jobli_employer_and_jobs_from_file(endpoint_url, auth_headers):
+    try:
+        project_dir: str = os.getenv('PROJECT_DIR')
+        job_test_file = open(f"{project_dir}/tests/resources/Jobli_jobs.json", encoding="UTF-8")
+        test_data = json.load(job_test_file)
+
+        # get existing employers in db table, later if the employer already exist the employer  wont be created
+        existing_employers: List[Employer] = get_exiting_employer_list(auth_headers, endpoint_url)
+        print(existing_employers)
+
+        # iterate the josn file, if employer already exist - skip insert, alway create a job
+        for employer_data in test_data:
+
+            employer: Employer = Employer.parse_obj(employer_data)
+            # check if employer already stored, skip insert if exist
+            persist_employer: Employer = get_employer_by_name(employer.business_name, existing_employers)
+            if not persist_employer:
+                # create employer
+                employer.employer_email = get_random_email(5, "@gmail.com")
+                employer.employer_terms = ["הודעה ראשונה", "הודעה שניה"]
+
+                employer.business_website = HttpUrl(f'https://www.global.com', scheme='https', host="global")
+
+                # address handling
+                address: Address = Address()
+                address.city = "פתח תקוה"
+                address.apartment= random.randint(1, 1000)
+                address.street = "ג׳בוטינסקי"
+                address.full_address = f"{address.city} {address.street} {address.apartment}"
+                employer.business_address = address
+
+                persist_employer = save_new_employer(auth_headers, employer, endpoint_url)
+                print(f"new object: {persist_employer}")
+
+            # create and store a job
+            job: EmployerJob = EmployerJob(**employer_data)
+            job.employer_id = persist_employer.employer_id
+
+            save_new_job(auth_headers, job, endpoint_url)
+    except IOError as exc:
+        print(f"error on data population:: validate the json file path")
+        assert False
+    finally:
+        job_test_file.close()
+
+
+def get_exiting_employer_list(auth_headers, endpoint_url) -> List[Employer]:
+    headers = {"Content-Type": "application/json"}
+    headers.update(auth_headers)
+    response = requests.api.get(url=f"{endpoint_url}/api/employers", headers=headers)
+    assert response.status_code == HTTPStatus.OK
+    employer_list: List[Employer] = []
+    employees_json = json.loads(response.text)["employers"]
+    for emp in employees_json:
+        employ = Employer(**json.loads(emp))
+        employer_list.append(employ)
+
+    return employer_list
+
+
+def save_new_employer(auth_headers, employer, endpoint_url) -> Employer:
+    headers = {"Content-Type": "application/json"}
+    headers.update(auth_headers)
+    response = requests.api.post(url=f"{endpoint_url}/api/employers", headers=headers, json=employer.dict())
+    assert response.status_code == HTTPStatus.CREATED
+    returned_employer: Employer = Employer.parse_obj(response.json())
+    assert returned_employer.employer_id
+    return returned_employer
+
+
+def save_new_job(auth_headers: dict, job: EmployerJob, endpoint_url: str) -> EmployerJob:
+    headers = {"Content-Type": "application/json"}
+    headers.update(auth_headers)
+    response = requests.api.post(url=f"{endpoint_url}/api/employers/{job.employer_id}/jobs", headers=headers, json=job.dict())
+    assert response.status_code == HTTPStatus.CREATED
+    returned_job: Employer = EmployerJob.parse_obj(response.json())
+    assert returned_job.job_id
+    return returned_job
 
 
 def test_create_jobli_employer(endpoint_url, auth_headers):
@@ -70,9 +166,6 @@ def test_update_jobli_employer(endpoint_url, auth_headers):
     value = "{\"employer_id\": \"261fe233-8da8-4dfd-af77-f5c7acba7409\", \"employer_email\": \"a@b.c\", \"business_name\": \"some business updated\", \"business_address\": {\"full_address\": \"some address in some city\", \"city\": null, \"street\": null, \"apartment\": null}, \"business_website\": \"https://www.new-website.com\", \"description\": \"The description changed!\", \"employer_terms\": [\"Term1\", \"Term2\", \"Term3\"], \"business_media\": null}"
 
     employer_value = Employer.parse_raw(value)
-
-
-
 
     # Create the employer
     employer = SOME_EMPLOYER
